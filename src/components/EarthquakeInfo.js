079213aeaa2ff5a85a1f9d6936577bc35eb1d22f
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-24 19:19:43 -0400

    corrected rendering issue,now viewable in browser

---
+++ src/components/EarthquakeInfo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+
+export default class earthquakeInfo extends Component {
+  render() {
+    return (
+      <div className="earthquake-title">
+        <h1>This is a list of 8 Earthquakes occuring on the morning of July 14th across the United States.</h1>
+      </div>
+    )
+  }
+}
